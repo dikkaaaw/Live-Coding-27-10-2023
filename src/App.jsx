8025813a242774949f7c4b61f9ab52f07f32e7ca
Dika Wicaksono <dhkwck00@gmail.com>
2023-10-29 18:04:50 +0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,9 @@
 import { useState } from "react";
 
-import Calculator from "./components/Calculator";
-import Header from "./components/Header";
-import Result from "./components/Result";
-
 function App() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 1000,
-    annualInvestment: 1500,
-    expectedReturn: 5,
-    duration: 10,
-  });
-
-  const durationIsValid = userInput.duration > 0;
-
-  function handleUserInput(inputKey, newValue) {
-    setUserInput((existingUserInput) => {
-      return {
-        ...existingUserInput,
-        [inputKey]: +newValue,
-      };
-    });
-  }
-
   return (
-    <>
-      <Header />
-      <Calculator onChangeProps={handleUserInput} userInput={userInput} />
-      {!durationIsValid && <p className="center">Masukkan Durasi diatas 0</p>}
-      {durationIsValid && <Result resultInput={userInput} />}
-    </>
-  );
+    <h1>React Investment Calculator </h1>
+  )
 }
 
 export default App;
