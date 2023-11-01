907b1d4a9eb9e91e580d17f094ff9509c3be366a
Dika Wicaksono <dhkwck00@gmail.com>
2023-11-01 15:14:59 +0700

    add some refactor code and fixing wrong code

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,29 +1,47 @@
-import "./Calculator.css"
+import "./Calculator.css";
 
-function Calculator({onChangeProps, userInput}) {
-    return <form id="user-input">
-        <div className="input-group">
-            <p>
-                <label>Inisial Investasi</label>
-                <input type="number" value={userInput.initialInvestment} onChange={(e) => onChangeProps(initialInvestment, e.target.value)}/>
-            </p>
-            <p>
-                <label>Investasi Tahunan</label>
-                <input type="number" value={userInput.annualInvestment} onChange={(e) => onChangeProps(annualInvestment, e.target.value)}/>
-            </p>
-        </div>
-        
-        <div className="input-group">
-            <p>
-                <label>Perkiraan balik modal</label>
-                <input type="number" value={userInput.expectedReturn} onChange={(e) => onChangeProps(expectedReturn, e.target.value)}/>
-            </p>
-            <p>
-                <label>Durasi</label>
-                <input type="number" value={userInput.duration} onChange={(e) => onChangeProps(duration, e.target.value)}/>
-            </p>
-        </div>
+function Calculator({ onChangeProps, userInput }) {
+  return (
+    <form id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Inisial Investasi</label>
+          <input
+            type="number"
+            value={userInput.initialInvestment}
+            onChange={(e) => onChangeProps("initialInvestment", e.target.value)}
+          />
+        </p>
+        <p>
+          <label>Investasi Tahunan</label>
+          <input
+            type="number"
+            value={userInput.annualInvestment}
+            onChange={(e) => onChangeProps("annualInvestment", e.target.value)}
+          />
+        </p>
+      </div>
+
+      <div className="input-group">
+        <p>
+          <label>Perkiraan balik modal</label>
+          <input
+            type="number"
+            value={userInput.expectedReturn}
+            onChange={(e) => onChangeProps("expectedReturn", e.target.value)}
+          />
+        </p>
+        <p>
+          <label>Durasi</label>
+          <input
+            type="number"
+            value={userInput.duration}
+            onChange={(e) => onChangeProps("duration", e.target.value)}
+          />
+        </p>
+      </div>
     </form>
+  );
 }
 
-export default Calculator
+export default Calculator;
